a6e2674bfc0945aeb1a9ea06b79048ae587b62c2
Alessandro David <alessandrodp2222@gmail.com>
2024-07-11 17:05:05 -0500

    episode 14: redux tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import './App.css';
-import {Person, Country} from "./components/Person";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <Router>
+                    <Link to="/"> Home </Link>
+                    <Link to="/login"> Login </Link>
+                    <Link to="/contact"> Contact </Link>
 
-  return (
-    <div className="App">
-        <Person
-            name="Alessandro"
-            email="alessandrodp2222@gmail.com"
-            age={21}
-            isMarried={true}
-            friends={["alan", "andre", "tyler", "josh", "jessica"]}
-            country={Country.France}
-        ></Person>
-    </div>
-  );
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </Router>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
